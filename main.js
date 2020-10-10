8d60a722a01baf93e047fcedff0f4b193ee23703
Kristen M <kristenmayorga@gmail.com>
2020-10-09 23:55:07 -0400

    added contact and fixed styling

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 // Initialize all div with carousel class
-const carousels = bulmaCarousel.attach(".carousel", options);
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
-// Loop on each carousel initialized
-for (var i = 0; i < carousels.length; i++) {
-  // Add listener to  event
-  carousels[i].on("before:show", (state) => {
-    console.log(state);
-  });
-}
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
 
-// Access to bulmaCarousel instance of an element
-const element = document.querySelector("#my-element");
-if (element && element.bulmaCarousel) {
-  // bulmaCarousel instance is available as element.bulmaCarousel
-  element.bulmaCarousel.on("before-show", function (state) {
-    console.log(state);
-  });
-}
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+});
